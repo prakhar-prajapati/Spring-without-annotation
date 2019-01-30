e38185893021da83bbf4178761e888acd3a9e696
PC-113 <PC-113@PC-113-PC>
2019-01-30 15:43:32 +0530

    complete Spring demo without using annotation

---
+++ SpringDemo/src/com/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		ModelAndView obj=new ModelAndView("hello");
+		obj.addObject("msg","Hello Spring");
+		
+		return obj;
+	}
+ 
+}
---
+++ SpringDemo/src/com/demo/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class WelcomeController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		ModelAndView obj=new ModelAndView("hello");
+		obj.addObject("msg","welcome Spring");
+		return obj;
+	}
+ 
+}
